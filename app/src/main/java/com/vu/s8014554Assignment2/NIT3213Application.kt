4875328f919f63ac647bdee945df9566b1a4c52a
Benjamin Morovan <147779284+halfpelican@users.noreply.github.com>
2026-09-12 14:32:18 +1000

    Enable Hilt in the application class

--- app/src/main/java/com/vu/s8014554Assignment2/NIT3213Application.kt
+++ app/src/main/java/com/vu/s8014554Assignment2/NIT3213Application.kt
@@ -2,7 +2,9 @@ package com.vu.s8014554Assignment2
 
 import android.app.Application
 import android.util.Log
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class NIT3213Application: Application() {
 
     override fun onCreate() {
